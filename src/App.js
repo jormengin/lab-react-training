b718f86dfd94ad82f2a6f580e70b6a0c6ab893a8
Jorge Mendez <jorge.mendezgines@gmail.com>
2023-03-03 15:56:35 +0100

    11 iteraton

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import DriverCard from './components/DriverCard';
 import LikeButton from './components/LikeButton';
 import ClickablePicture from './components/ClickablePicture';
 import Dice from './components/Dice';
+import Carousel from './components/Carousel';
 
 function App() {
   return (
@@ -107,6 +108,15 @@ function App() {
       <ClickablePicture img="maxence.png" imgClicked="maxence-glasses.png" />
       <h1> Tenth Iteration</h1>
       <Dice />
+      <h1> Eleventh Iteration</h1>
+      <Carousel
+        images={[
+          'https://randomuser.me/api/portraits/women/1.jpg',
+          'https://randomuser.me/api/portraits/men/1.jpg',
+          'https://randomuser.me/api/portraits/women/2.jpg',
+          'https://randomuser.me/api/portraits/men/2.jpg',
+        ]}
+      />
     </div>
   );
 }
